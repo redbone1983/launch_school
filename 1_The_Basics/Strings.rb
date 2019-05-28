60239732ed51d75b3bb8e291b5e41afb27e2a5ed
Maya Roman <maya.roman7@gmail.com>
2019-05-28 16:01:26 -0700

    Spend to long on factorial exercise

--- 1_The_Basics/Strings.rb
+++ 1_The_Basics/Strings.rb
@@ -15,6 +15,49 @@ puts 7312 % 10
 
 # Exercise 3
 
+movies = {
+	yentl: 1983,
+	gremlins: 1984,
+	et: 1982,
+	harry_and_the_hendersons: 1987,
+	moonwalker: 1988,
+}
+
+puts movies[:yentl]
+puts movies[:gremlins]
+puts movies[:et]
+puts movies[:harry_and_the_hendersons]
+puts movies[:moonwalker]
+
+# Exercise 4 
+
+movies_arr = [
+	1983,
+	1984,
+	1982,
+	1987,
+	1988
+]
+
+puts movies_arr[0]
+puts movies_arr[1]
+puts movies_arr[2]
+puts movies_arr[3]
+puts movies_arr[4]
+
+# Exercise 5
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+ 
+# 7352
+
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 3 * 2 * 1
+puts 5 * 4 * 3 * 2 * 1
+puts 2 * 1
+
+# Will refactor later using a recursive method
+
+
 
 
 
