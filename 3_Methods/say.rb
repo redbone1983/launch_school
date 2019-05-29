b15f39c17da0fe89ad6edbc894abe005d3eaaacd
Maya Roman <maya.roman7@gmail.com>
2019-05-28 22:42:16 -0700

    Add notes on Method Definition/Local Variable Scope

--- 3_Methods/say.rb
+++ 3_Methods/say.rb
@@ -10,3 +10,11 @@ say("hi")
 say("how are you")
 say("I'm fine")
 
+# Method Definition and Local Variable Scope
+
+# A method definition creates its own scope outside the regular flow of execution. 
+
+# This is why local variables within a method definition cannot be referenced from outside of the method definition. 
+
+# It's also the reason why local variables within a method definition cannot access data outside of the method definition (unless the data is passed in as a parameter).
+
