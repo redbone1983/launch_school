0b21d272dcbd45fcbeca94910ccb2c69dfef9958
Maya Roman <maya.roman7@gmail.com>
2019-05-28 22:35:52 -0700

    Add default parameters

--- 3_Methods/say.rb
+++ 3_Methods/say.rb
@@ -1,10 +1,11 @@
 # Methods
+# Adding a default parameter structures a method definition to work regardless of if you pass it an argument or not.
 
-def say(words)
+def say(words='hello')
   puts words + '.'
 end
 
-say("hello")
+say()
 say("hi")
 say("how are you")
 say("I'm fine")
