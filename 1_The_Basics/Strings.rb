a277d715fded7cc7f7534b7af672a853625bb451
Maya Roman <maya.roman7@gmail.com>
2019-05-28 17:11:02 -0700

    Fix indent spacing

--- 1_The_Basics/Strings.rb
+++ 1_The_Basics/Strings.rb
@@ -48,8 +48,6 @@ puts movies_arr[4]
 # Exercise 5
 # Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
  
-# 7352
-
 puts 7 * 6 * 5 * 4 * 3 * 2 * 1
 puts 3 * 2 * 1
 puts 5 * 4 * 3 * 2 * 1
