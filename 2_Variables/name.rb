8c7a04f6f9e1e2dc2b38eecacc7c015817a8c5e3
Maya Roman <maya.roman7@gmail.com>
2019-05-28 19:15:54 -0700

    Update name.rb

--- 2_Variables/name.rb
+++ 2_Variables/name.rb
@@ -12,3 +12,17 @@ puts "Hello, #{user_input}. I hope you are having a swell day!"
 # Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row.
 
 10.times { puts "Hello, #{user_input}!" }
+
+# Exercise 4
+
+# Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
+
+print "What is your first name? "
+first_name = gets.chomp
+
+print "What is your last name? "
+last_name = gets.chomp
+
+puts "Hello, #{first_name} #{last_name}. I hope you are having a swell day!"
+
+
