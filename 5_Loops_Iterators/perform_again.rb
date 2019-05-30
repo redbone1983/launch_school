aaa0aa920cdde7b4e996949372ca76223e1ab2ec
Maya Roman <maya.roman7@gmail.com>
2019-05-30 13:32:12 -0700

    Add second do/while construct

--- 5_Loops_Iterators/perform_again.rb
+++ 5_Loops_Iterators/perform_again.rb
@@ -1,9 +1,18 @@
 # do/while loop 
 
-loop do
-  puts "Do you want to do that again?"
+# loop do
+#   puts "Do you want to do that again?"
+#   answer = gets.chomp
+#   if answer != 'Y'
+#     break
+#   end
+# end
+
+# Side note: there's also another construct in Ruby that supports "do/while" loops, like this:
+
+begin
+  puts "Would you like another go of it?"
   answer = gets.chomp
-  if answer != 'Y'
-    break
-  end
-end
+end while answer == 'Y'
+
+
