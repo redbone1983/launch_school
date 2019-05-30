21ee02e6acdd00fe03cc5d99bb829dddad21c9a8
Maya Roman <maya.roman7@gmail.com>
2019-05-30 14:29:57 -0700

    Modify conditional_while_loop_with_next.rb

--- 5_Loops_Iterators/conditional_while_loop_with_next.rb
+++ 5_Loops_Iterators/conditional_while_loop_with_next.rb
@@ -9,3 +9,16 @@ while x <= 10
   end
   x += 1
 end
+
+# conditional_while_loop_with_break.rb
+
+x = 0
+
+while x <= 10
+  if x == 7
+    break # thread of execution departs here
+  elsif x.odd?
+    puts x
+  end
+  x += 1
+end
