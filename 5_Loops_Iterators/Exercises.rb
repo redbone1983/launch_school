71b0e1af42686e4eb3d8d08b726c6f3e6c970a96
Maya Roman <maya.roman7@gmail.com>
2019-05-30 15:19:20 -0700

    Add Exercises.rb

---
+++ 5_Loops_Iterators/Exercises.rb
@@ -0,0 +1,38 @@
+# # Exercise 1
+# # => [1, 2, 3, 4, 5]
+
+
+# # x = [1, 2, 3, 4, 5]
+# # x.each do |a|
+# #   a + 1
+# # end
+
+# # Exercise 2
+
+# def pester(user_input)
+#   while user_input != "STOP"
+#     p "Please Enter STOP and I will leave you alone. " 
+#     user_input = gets.chomp
+#   end
+# end
+
+# p "Please Enter STOP and I will leave you alone. " 
+# user_input = gets.chomp
+
+# pester(user_input)
+
+# # Exercise 4 
+
+# names = ['Amanda', 'Gilda', 'Maya', 'Anne', 'Ann', 'Argus']
+
+# names.each_with_index{ |name, i| puts "#{i}: #{name}" }
+
+# Exercise 5 
+def countdown(num)
+    puts num
+  if num != 0
+    countdown(num - 1) 
+  end
+end
+ 
+countdown(10)
