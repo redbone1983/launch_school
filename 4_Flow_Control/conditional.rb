ace83c56b18f8541f79f9f073257353c25cef8fb
Maya Roman <maya.roman7@gmail.com>
2019-05-29 17:21:40 -0700

    Add conditional.rb and case_statement.rb files

---
+++ 4_Flow_Control/conditional.rb
@@ -0,0 +1,21 @@
+# When you are writing programs, you want your data to make the right decisions. You want your data to do the right thing when it's supposed to. In computer programming, this is called conditional flow.
+
+# How do we make data do the right thing? We use:
+# Conditionals.
+# A conditional is a fork (or many forks) in the road. Your data approaches a conditional and the conditional then tells the data where to go based on some defined parameters. 
+
+# puts "Put in a number"
+# a = gets.chomp.to_i
+
+# if a == 3
+#   puts "a is 3"
+# elsif a == 4
+#   puts "a is 4"
+# else
+#   puts "a is neither 3, nor 4"
+# end
+
+# Ruby ternary operation looks the same as a Javascript one
+p true ? "This is true" : "This is false" # => "This is true"
+p false ? "This is true" : "This is false" # => "This is false"
+
