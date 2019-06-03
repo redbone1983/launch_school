d1bf0f41cfa133fe6ade35ae2bd0e9b4b5d5237d
Maya Roman <maya.roman7@gmail.com>
2019-06-02 23:50:44 -0700

    Add ex_7.rb

---
+++ 9_Exercises/ex_7.rb
@@ -0,0 +1,11 @@
+# Exercise 7
+# What's the major difference between an Array and a Hash?
+
+# An Array is a collection of data with elements that can be accessed by its index, starting with 0, 1, 2, 3...
+
+
+# A Hash is a collection of data where each element is arranged in key: value pairs where the value can be accessed by its key
+
+
+
+
