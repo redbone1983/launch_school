5ae7696b3a0feb681555a3e49ae73b591e00da07
Maya Roman <maya.roman7@gmail.com>
2019-06-02 22:31:09 -0700

    Add ex_3.rb

---
+++ 9_Exercises/ex_3.rb
@@ -0,0 +1,10 @@
+# Exercise 3.
+#  Now, using the same array from #2, use the select method to extract all odd numbers into a new array.
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_arr = []
+arr.select do |num| 
+  if num % 2 != 0 
+    new_arr.push(num)
+  end
+end
+p new_arr
