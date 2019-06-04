be10285cd30d2ee9497f467740468e854b6a915d
Maya Roman <maya.roman7@gmail.com>
2019-06-04 09:12:48 -0700

    Update ex_15.rb

--- 9_Exercises/ex_15.rb
+++ 9_Exercises/ex_15.rb
@@ -3,5 +3,13 @@
 
 arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
 
-p arr.delete_if {|string| string.start_with?("s") }
+p arr.delete_if {|string| string.start_with?("s")}
 #=> ["winter", "ice", "white trees"]
+
+# Then recreate the arr and get rid of all of the words that start with "s" or starts with "w".
+og_arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+p og_arr.delete_if {|string| string.start_with?("s", "w")}
+
+
+
