b66763930b14d08ca423251a733088dfb1b8cb02
Maya Roman <maya.roman7@gmail.com>
2019-06-04 09:53:06 -0700

    Rename files to reorder prefix numbers

---
+++ 09_Exercises/ex_11.rb
@@ -0,0 +1,4 @@
+# Exercise 11.
+# Look at several Rails/Ruby online API sources and say which one you like best and why.
+
+# Out of all the ones I could find, I found www.ruby-docs.org AND APIDOCK to be the most comprehensive for ruby documentation. I do think that API has a better buitlin search bar. ruby-docs 
--- 9_Exercises/ex_11.rb
+++
@@ -1,4 +0,0 @@
-# Exercise 11.
-# Look at several Rails/Ruby online API sources and say which one you like best and why.
-
-# Out of all the ones I could find, I found www.ruby-docs.org to be the most comprehensive and well designed resource for ruby documentation"""
