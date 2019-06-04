0d06ab5a4d1f48895fc9673e0779360983fb12db
Maya Roman <maya.roman7@gmail.com>
2019-06-04 09:27:53 -0700

    Add ex_17.rb

---
+++ 9_Exercises/ex_17.rb
@@ -0,0 +1,14 @@
+# Exercise 17.
+# What will the following program output?
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+
+# "These hashes are the same!"
+# Both hashes have the same values but just use different syntax.
