912f61118648cc50a8db580026e7da27bc7c0262
Maya Roman <maya.roman7@gmail.com>
2019-06-10 11:41:41 -0700

    Fix string interpolation in .yml

--- 02_RB_101/Lesson_02/calculator_walk_through.rb
+++ 02_RB_101/Lesson_02/calculator_walk_through.rb
@@ -1,7 +1,6 @@
 require "yaml"
 MESSAGES = YAML.load_file("calculator_messages.yml")
 
-
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -30,10 +29,10 @@ end
 # Welcome users
 name = ""
 loop do
-  prompt(MESSAGES["mes_01"])
+  prompt("Thank you for using calculator! Enter your name: ")
   name = Kernel.gets().chomp()
   if valid_string?(name)
-    prompt(MESSAGES["mes_02"])
+    prompt(MESSAGES["mes_02"] + "#{name}.")
   else
     prompt(MESSAGES["mes_03"])
   end
@@ -45,11 +44,9 @@ count = 0
 loop do
   number1 = ""
   loop do
-    # welcome1 = "Welcome back, #{name}. You have calculated #{count} time."
-    # welcome2 = "Welcome back, #{name}. You have calculated #{count} times."
     # Use Ternary Operator for optional greeting for return users
-    count == 1 ? prompt(MESSAGES["mes_04"]) : nil
-    count > 1 ? prompt(MESSAGES["mes_05"]) : nil
+    count == 1 ? prompt(MESSAGES["mes_04"] + "#{name}.") : nil
+    count > 1 ? prompt(MESSAGES["mes_04"] + "#{name}.") : nil
     # Prompt user for number 1
     prompt(MESSAGES["mes_06"])
     number1 = Kernel.gets().chomp()
@@ -76,21 +73,20 @@ loop do
 
   operator = ""
   loop do
-  #   operator_prompt = <<-MSG
-  #   "What operation would you like to perform?
-  #   1) add
-  #   2) subtract
-  #   3) multiply
-  #   4) divide
-  # MSG
-    prompt(MESSAGES["mes_09"])
+    operator_prompt = <<-MSG
+    "What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+    prompt(operator_prompt)
     operator = Kernel.gets().chomp()
-
     if %w(1 2 3 4).include?(operator)
       prompt("#{operation_to_message(operator)} the two numbers...")
       break
     else
-      prompt("Must choose 1, 2, 3 or 4.")
+      prompt(MESSAGES["mes_10"])
     end
   end
 
@@ -104,10 +100,10 @@ loop do
       number1.to_i() * number2.to_i()
     when "4"
       number1.to_f() / number2.to_f()
-    else "The operator you entered is invalid."
+    else MESSAGES["mes_11"]
     end
 
-  prompt(MESSAGES["mes_12"])
+  prompt(MESSAGES["mes_12"] + "#{result}")
 
   prompt(MESSAGES["mes_13"])
   answer = Kernel.gets().chomp()
@@ -119,4 +115,4 @@ loop do
   end
 end
 
-prompt(MESSAGES["mes_14"])
+prompt(MESSAGES["mes_14"] + "#{name}!")
