2d5d3d97f72d740bde4476e8f4e1e81c4ba5b500
Maya Roman <maya.roman7@gmail.com>
2019-06-11 12:35:31 -0700

    Rubocop Fixes

--- 02_RB_101/Lesson_02/mortgage_loan_calc.rb
+++ 02_RB_101/Lesson_02/mortgage_loan_calc.rb
@@ -1,29 +1,29 @@
-# Take everything you've learned so far and build a mortgage calculator (or car payment calculator -- it's the same thing).
+# Take everything you've learned so far and build a mortgage calculator
+# (or car payment calculator -- it's the same thing).
 # Translated to Ruby, this is what the formula looks like:
 # m = p * (j / (1 - (1 + j)**(-n)))
+
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts "=> #{message}"
 end
 
 def valid_string?(str)
-  str.class == String
+  str != String ? str.to_s : nil
 end
 
 def valid_number?(num)
-  num.to_i.class == Integer
-  num.to_i
+  num != Integer ? num.to_i : nil
 end
 
 def valid_float?(apr_num)
-   apr_num.to_f == Float
-   apr_num.to_f
+  apr_num != Float ? apr_num.to_f : nil
 end
 
 # User Greeting
 name = ""
 loop do
-prompt "Welcome to Mortgage Calculator. Please enter your name: "
-name = gets.chomp
+  prompt "Welcome to Mortgage Calculator. Please enter your name: "
+  name = gets.chomp
   if valid_string?(name)
     prompt "Hello, #{name}."
   else
@@ -35,8 +35,9 @@ end
 
 # Main Loop Starts Here
 loop do
-  # GET loan amount from user
+  # Init loan variable
   loan = ""
+  # GET loan amount from user
   loop do
     prompt "Please enter loan amount: "
     loan = gets.chomp
@@ -92,25 +93,20 @@ loop do
   end
 
   # Calculate monthly payment operation
-  
   # 1. Convert Yearly Term to Monthly Term
   monthly_term = term.to_i * 12
-  
+
   # 2. Convert Yearly APR to Monthly APR
   monthly_apr = (apr.to_f * 0.010) / 12
 
   # 3. Apply <m = p * (j / (1 - (1 + j)**(-n)))> formula
   # and save value to result variable
-  result = loan * (monthly_apr / (1 - (1 + monthly_apr)**(-monthly_term)))
+  result = loan * (monthly_apr / (1 - (1 + monthly_apr)**-monthly_term))
   # 4. Round result 2 decimal places
   result = result.round(2)
-  
+
   # Output Results to user
   prompt("Your estimated monthly payment is $#{result}.")
-
   # Stop Main Loop. End Program.
   break
 end
-
-# Finally, don't forget to run your code through Rubocop.
-
