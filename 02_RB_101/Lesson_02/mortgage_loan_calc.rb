ba7392f80c28eedac0fd701fbaf0daf934c40671
Maya Roman <maya.roman7@gmail.com>
2019-06-11 13:16:32 -0700

    Refactor methods

--- 02_RB_101/Lesson_02/mortgage_loan_calc.rb
+++ 02_RB_101/Lesson_02/mortgage_loan_calc.rb
@@ -3,27 +3,32 @@
 # Translated to Ruby, this is what the formula looks like:
 # m = p * (j / (1 - (1 + j)**(-n)))
 
+# Declare methods here
 def prompt(message)
   puts "=> #{message}"
 end
 
 def valid_string?(str)
-  str != String ? str.to_s : nil
+  str.class == String
 end
 
 def valid_number?(num)
-  num != Integer ? num.to_i : nil
+  num.class == Integer
 end
 
 def valid_float?(apr_num)
-  apr_num != Float ? apr_num.to_f : nil
+  apr_num.class == Float
 end
 
-# User Greeting
+def round_by_2(long_num)
+  long_num.round(2)
+end
+
+# User Greeting Loop
 name = ""
 loop do
   prompt "Welcome to Mortgage Calculator. Please enter your name: "
-  name = gets.chomp
+  name = gets.chomp.to_s
   if valid_string?(name)
     prompt "Hello, #{name}."
   else
@@ -40,8 +45,7 @@ loop do
   # GET loan amount from user
   loop do
     prompt "Please enter loan amount: "
-    loan = gets.chomp
-    loan = loan.to_i
+    loan = gets.to_i
     # Validate User Input
     if valid_number?(loan)
       prompt "Great. You have entered a loan amount of #{loan}."
@@ -55,7 +59,7 @@ loop do
   apr = ""
   loop do
     prompt "Please enter Annual Percentage Rate amount: "
-    apr = gets.chomp
+    apr = gets.to_f
     # Validate User Input
     if valid_float?(apr)
       prompt "Great. You have entered an APR of #{apr}%."
@@ -69,7 +73,7 @@ loop do
   term = ""
   loop do
     prompt "Now, please enter duration in years of loan term: "
-    term = gets.chomp
+    term = gets.to_i
     # Validate User Input
     if valid_number?(term)
       prompt "Great. You have entered a loan term length of #{term} years."
@@ -79,7 +83,7 @@ loop do
       prompt "Please enter a valid number."
     end
   end
-  # Make calculation Prompt?
+  # Make calculation Prompt
   operator = ""
   loop do
     prompt("Please enter 'Y' to calculate monthly payment.")
@@ -94,16 +98,16 @@ loop do
 
   # Calculate monthly payment operation
   # 1. Convert Yearly Term to Monthly Term
-  monthly_term = term.to_i * 12
+  monthly_term = term * 12
 
   # 2. Convert Yearly APR to Monthly APR
-  monthly_apr = (apr.to_f * 0.010) / 12
+  monthly_apr = (apr * 0.010) / 12
 
   # 3. Apply <m = p * (j / (1 - (1 + j)**(-n)))> formula
   # and save value to result variable
-  result = loan * (monthly_apr / (1 - (1 + monthly_apr)**-monthly_term))
+ monthly_payment = loan * (monthly_apr / (1 - (1 + monthly_apr)**-monthly_term))
   # 4. Round result 2 decimal places
-  result = result.round(2)
+  result = round_by_2(monthly_payment)
 
   # Output Results to user
   prompt("Your estimated monthly payment is $#{result}.")
