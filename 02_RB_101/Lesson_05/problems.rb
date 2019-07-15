d6ed8b49c0946e8def028b205bec7bd490ea3ade
Maya Roman <maya.roman7@gmail.com>
2019-07-15 19:59:07 +0000

    Add ex 1-3

--- 02_RB_101/Lesson_05/problems.rb
+++ 02_RB_101/Lesson_05/problems.rb
@@ -1,22 +1,20 @@
-# Practice Problem 1
+# arr = ['10', '11', '9', '7', '8']
 
-# How would you order this array of number strings by descending numeric value?
+# p arr.sort { |a, b| b.to_i <=> a.to_i }
 
-arr = ['10', '11', '9', '7', '8']
+# # Practice Problem 2
 
-arr.sort { |a, b| b.to_i <=> a.to_i }
+# # How would you order this array of hashes based on the year 
+# # of publication of each book, from the earliest to the latest?
 
-# Practice Problem 2
+# books = [
+#   {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
+#   {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
+#   {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
+#   {title: 'Ulysses', author: 'James Joyce', published: '1922'}
+# ]
 
-# How would you order this array of hashes based on the year 
-# of publication of each book, from the earliest to the latest?
-
-books = [
-  {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
-  {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
-  {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
-  {title: 'Ulysses', author: 'James Joyce', published: '1922'}
-]
+# p books.sort_by { |hsh| hsh[:published] }
 
 # Practice Problem 3
 
@@ -25,14 +23,23 @@ books = [
 
 arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
 
+p arr1[2][1][3]
+
 arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+
+p arr2[1][:third][0]
 
 arr3 = [['abc'], ['def'], {third: ['ghi']}]
 
+p arr3[2][:third][0][0]
+
 hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
+
+p hsh1['b'][1]
 
 hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
 
+p hsh2[:third].keys.join
 # Practice Problem 4
 
 # For each of these collection objects where the value 3 occurs, 
