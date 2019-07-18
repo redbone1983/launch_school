b043cdc8a9c78a7ae12b78656c028bd7365222ea
Maya Roman <maya.roman7@gmail.com>
2019-07-18 21:50:35 +0000

    Add rubocop logic fixes

--- 02_RB_101/Lesson_06/ttt_walkthrough.rb
+++ 02_RB_101/Lesson_06/ttt_walkthrough.rb
@@ -1,4 +1,7 @@
 # Initialize Game Constants
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -7,7 +10,7 @@ def prompt(str)
   puts "=> #{str}"
 end
 
-# Output board view to the console
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -25,6 +28,7 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 # Create hash data structure to store board's state
 def initialize_board
@@ -82,18 +86,10 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]] # diagonals
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-      return "you"
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).all?(PLAYER_MARKER)
+      return "You"
+    elsif brd.values_at(line[0], line[1], line[2]).all?(COMPUTER_MARKER)
       return "Computer"
     end
   end
